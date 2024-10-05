bd5892283cdf1d04e8626c6c8abd4001360d1f5d
Alex Olteanu <alexandruolteanu42@gmail.com>
2024-10-05 09:37:56 +0100

    Decode bencoded integers

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,5 +1,7 @@
 use std::num::ParseIntError;
 
+pub type DecodeResult = Result<serde_json::Value, DecodeError>;
+
 #[derive(Debug)]
 pub struct DecodeError;
 
@@ -9,17 +11,18 @@ impl From<ParseIntError> for DecodeError {
     }
 }
 
-pub fn decode(value: &str) -> Result<serde_json::Value, DecodeError> {
+pub fn decode(value: &str) -> DecodeResult {
     let mut chars = value.chars();
     let first_char = chars.next();
 
     match first_char {
         Some(first_char) if first_char.is_ascii_digit() => decode_string(value),
+        Some('i') => decode_integer(value),
         _ => Err(DecodeError),
     }
 }
 
-fn decode_string(value: &str) -> Result<serde_json::Value, DecodeError> {
+fn decode_string(value: &str) -> DecodeResult {
     let mut part_iter = value.splitn(2, ':');
 
     let s_length = part_iter.next().ok_or(DecodeError)?;
@@ -28,5 +31,13 @@ fn decode_string(value: &str) -> Result<serde_json::Value, DecodeError> {
     let s = part_iter.next().ok_or(DecodeError)?;
     let s = &s[..s_length];
 
-    Ok(serde_json::Value::String(s.to_string()))
+    Ok(serde_json::Value::String(s.into()))
+}
+
+fn decode_integer(value: &str) -> DecodeResult {
+    let e_index = value.find('e').ok_or(DecodeError)?;
+    let integer = &value[1..e_index];
+    let integer: i32 = integer.parse()?;
+
+    Ok(serde_json::Value::Number(integer.into()))
 }
